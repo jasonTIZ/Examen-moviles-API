4c541487acda58f37cf31c3a831b118bc6d6bc74
Jeycob Barrientos García <jeycobbarrientosgarcia@gmail.com>
2025-05-15 00:00:13 -0600

    Finalizando lo necesario para la implementación de estudiante

--- api/Dtos/Course/CourseDto.cs
+++ api/Dtos/Course/CourseDto.cs
@@ -5,5 +5,8 @@ namespace api.Dtos.Course
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Schedule { get; set; }
+        public string Professor { get; set; }
     }
 }
